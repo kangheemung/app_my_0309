06f71901af3fc4a7bbda61d32eb4f4cd51de95ee
kurotamashi30 <kurotamashi30@gmail.com>
2024-03-09 20:08:58 +0900

    app_create

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect=(() => {
+    async function fetchData() {
+      try{
+        const response =await fetch('https://api.example.com/data')
+        const json = await response.json();
+        setData(json);
+      } catch (error) {
+          console.error("Error fetching data: ", error);
+      }
+    }
+        fetchData();
+    }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
     </div>
   );
 }
